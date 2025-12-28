b6ecaaf94bccd3910b6e0796cbdfcc6002705ed5
Christian Budde <Christian.Budde@meko.de>
2025-12-28 02:38:44 +0100

    feat: further optimizations

--- internal/fft/dit_bench_test.go
+++ internal/fft/dit_bench_test.go
@@ -170,6 +170,90 @@ func BenchmarkDIT16_Generic_Complex128(b *testing.B) {
 	}
 }
 
+func BenchmarkDIT32_Specialized_Complex64(b *testing.B) {
+	const n = 32
+	src := make([]complex64, n)
+	dst := make([]complex64, n)
+	scratch := make([]complex64, n)
+	twiddle := ComputeTwiddleFactors[complex64](n)
+	bitrev := ComputeBitReversalIndices(n)
+
+	for i := range src {
+		src[i] = complex(float32(i), float32(-i))
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.SetBytes(int64(n * 8))
+
+	for range b.N {
+		forwardDIT32Complex64(dst, src, twiddle, scratch, bitrev)
+	}
+}
+
+func BenchmarkDIT32_Generic_Complex64(b *testing.B) {
+	const n = 32
+	src := make([]complex64, n)
+	dst := make([]complex64, n)
+	scratch := make([]complex64, n)
+	twiddle := ComputeTwiddleFactors[complex64](n)
+	bitrev := ComputeBitReversalIndices(n)
+
+	for i := range src {
+		src[i] = complex(float32(i), float32(-i))
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.SetBytes(int64(n * 8))
+
+	for range b.N {
+		ditForward[complex64](dst, src, twiddle, scratch, bitrev)
+	}
+}
+
+func BenchmarkDIT32_Specialized_Complex128(b *testing.B) {
+	const n = 32
+	src := make([]complex128, n)
+	dst := make([]complex128, n)
+	scratch := make([]complex128, n)
+	twiddle := ComputeTwiddleFactors[complex128](n)
+	bitrev := ComputeBitReversalIndices(n)
+
+	for i := range src {
+		src[i] = complex(float64(i), float64(-i))
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.SetBytes(int64(n * 16))
+
+	for range b.N {
+		forwardDIT32Complex128(dst, src, twiddle, scratch, bitrev)
+	}
+}
+
+func BenchmarkDIT32_Generic_Complex128(b *testing.B) {
+	const n = 32
+	src := make([]complex128, n)
+	dst := make([]complex128, n)
+	scratch := make([]complex128, n)
+	twiddle := ComputeTwiddleFactors[complex128](n)
+	bitrev := ComputeBitReversalIndices(n)
+
+	for i := range src {
+		src[i] = complex(float64(i), float64(-i))
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.SetBytes(int64(n * 16))
+
+	for range b.N {
+		ditForward[complex128](dst, src, twiddle, scratch, bitrev)
+	}
+}
+
 func BenchmarkDIT64_Specialized_Complex64(b *testing.B) {
 	const n = 64
 	src := make([]complex64, n)
